c00d40ce410491b58b05ba6749952d8841965420
Kafy Dier <133913272+Adhrit-Verma@users.noreply.github.com>
2023-05-26 00:27:12 +0530

    Update engine.py

--- loadup/engine.py
+++ loadup/engine.py
@@ -5,11 +5,22 @@ import re
 from gtts import gTTS
 from playsound import playsound
 
-class Engine:
-    def __init__(self):
-        self.ear = sr.Recognizer()
-        self.is_listening = False
-        self.trigger_name = 'maya'
+class CommandProcessor:
+    def __init__(self, trigger_name):
+        self.trigger_name = trigger_name
+
+    def process_command(self, command):
+        if re.search(r'\b' + self.trigger_name + r'\b', command):
+            command = re.sub(r'\b' + self.trigger_name + r'\b', '', command)
+            if 'play' in command:
+                command = command.replace('play', 'playing')
+                song = command + ''
+                self.talk(song)
+                pywhatkit.playonyt(song)
+            elif 'who are you' in command:
+                response = 'Hey! My name is Maya. I am an Artificial Intelligence designed by Kafy Dier aka Adhrit Verma. ' \
+                           'I am the very first build of his 1st generation versions of full-fledged AIs'
+                self.talk(response)
 
     def talk(self, text):
         try:
@@ -19,19 +30,23 @@ class Engine:
         except Exception as e:
             print(f"Error: {e}")
 
+
+class Engine:
+    def __init__(self, trigger_name):
+        self.ear = sr.Recognizer()
+        self.is_listening = False
+        self.trigger_name = trigger_name
+
     def set_microphone_threshold(self):
         try:
             with sr.Microphone() as mic:
-                print('\nCalibrating microphone sensitivity...')
                 self.ear.adjust_for_ambient_noise(mic, duration=1)
-                print('Microphone sensitivity calibrated.')
         except Exception as e:
             print(f"Error: {e}")
 
     def take_command(self):
         try:
             with sr.Microphone() as mic:
-                print('\nListening...\n')
                 self.ear.dynamic_energy_adjustment_ratio = 1.5  # Adjust this ratio to control dynamic energy adjustment
                 voice = self.ear.listen(mic)
                 command = self.ear.recognize_google(voice)
@@ -45,36 +60,28 @@ class Engine:
             print(f"Error: {e}")
         return ''
 
-    def process_command(self, command):
-        if re.search(r'\b' + self.trigger_name + r'\b', command):
-            command = re.sub(r'\b' + self.trigger_name + r'\b', '', command)
-            if 'play' in command:
-                command = command.replace('play', 'playing')
-                song = command + ''
-                self.talk(song)
-                pywhatkit.playonyt(song)
-            elif 'who are you' in command:
-                print('Hey! My name is Maya. I am an Artificial Intelligence designed by Kafy Dier aka Adhrit Verma. '
-                      'I am the very first build of his 1st generation versions of full-fledged AIs')
-                self.talk('Hey! My name is Maya. I am an Artificial Intelligence designed by Kafy Dier aka Adhrit Verma. '
-                          'I am the very first build of his 1st generation versions of full-fledged AIs')
-
-    def listening_loop(self):
+    def listening_loop(self, command_processor):
         while self.is_listening:
             command = self.take_command()
             if command:
-                self.process_command(command)
+                command_processor.process_command(command)
 
     def run_maya(self):
         self.set_microphone_threshold()
         self.is_listening = True
-        listening_thread = threading.Thread(target=self.listening_loop)
+        command_processor = CommandProcessor(self.trigger_name)
+        listening_thread = threading.Thread(target=self.listening_loop, args=(command_processor,))
         listening_thread.start()
         self.talk("Maya is ready.")
 
+    def talk(self, text):
+        try:
+            tts = gTTS(text=text, lang='en')
+            tts.save('speech.mp3')
+            playsound('speech.mp3')
+        except Exception as e:
+            print(f"Error: {e}")
 
-engine = Engine()
+
+engine = Engine(trigger_name='maya')
 engine.run_maya()
-# Keep the main thread running to perform other tasks or use time.sleep() to pause execution
-while True:
-    pass
