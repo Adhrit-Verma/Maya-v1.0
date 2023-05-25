0c1d0d583413ab039343d275355f4bc50e96e2e4
Kafy Dier <133913272+Adhrit-Verma@users.noreply.github.com>
2023-05-25 12:33:38 +0530

    removed loading bar

--- loadup/handler.py
+++ loadup/handler.py
@@ -1,9 +1,27 @@
 import os
+import time
 class handler:
 
-    def params(self):
-        self.command=""
-        self.detail=""
+    def __init__(self):
+        self.load_cNd()
     
+    def load_cNd(self):
+        root=os.getcwd()
+        self.cmds=(f"cd {root}","cd A.D.A/loadup/bin","start ada_ascii_art.bat",)
+        #print(self.cmds)
+
     def execute(self):
-        os.system(f"{self.command} {self.detail}")
+        for item in self.cmds:
+            (c)=item
+            if "ada_ascii_art.bat" in c:
+                timeout_sec=15
+                start_time=time.time()
+                while True:
+                    os.system(f"{c}")
+                    if time.time() - start_time >= timeout_sec:
+                        os.system("start terminate.bat")
+                        break
+            else:
+                os.system(f"{c}")
+
+control=handler()
