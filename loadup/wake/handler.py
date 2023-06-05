358a3597187afad0c46990e4476d82ad86ea9d62
Kafy Dier <133913272+Adhrit-Verma@users.noreply.github.com>
2023-06-05 12:40:36 +0530

    Update handler.py

--- loadup/wake/handler.py
+++ loadup/wake/handler.py
@@ -1,6 +1,13 @@
 import os
 import sys
 import subprocess
+import platform
+import socket
+import psutil
+import json
+import threading
+import geocoder
+
 
 class handler:
 
@@ -34,12 +41,68 @@ class handler:
             print("File not found.")
         except PermissionError:
             print("Permission denied.")
+
+    def gather_system_info(self):
+        system_info = {}
+
+        # Get OS information
+        system_info['os_name'] = platform.system()
+        system_info['os_version'] = platform.release()
+
+        # Get hostname
+        system_info['hostname'] = socket.gethostname()
+
+        # Get CPU information
+        cpu_info = {}
+        cpu_info['cpu_count'] = psutil.cpu_count(logical=False)
+        cpu_info['cpu_threads'] = psutil.cpu_count(logical=True)
+        cpu_info['cpu_usage'] = psutil.cpu_percent(interval=1)
+        system_info['cpu_info'] = cpu_info
+
+        # Get memory information
+        memory_info = {}
+        memory_info['total_memory'] = psutil.virtual_memory().total
+        memory_info['available_memory'] = psutil.virtual_memory().available
+        system_info['memory_info'] = memory_info
+
+        # Get disk information
+        disk_info = {}
+        disk_usage = psutil.disk_usage('/')
+        disk_info['total_space(MB)'] = disk_usage.total // (1024 * 1024)
+        disk_info['free_space(MB)'] = disk_usage.free // (1024 * 1024)
+        system_info['disk_info'] = disk_info
+
+        # Get number of drives
+        drive_info = {}
+        drives = psutil.disk_partitions(all=True)
+        drive_info['num_drives'] = len(drives)
+        system_info['drive_info'] = drive_info
+
+        # Get PC location
+        pc_location = {}
+        location = geocoder.ip('me').latlng
+        pc_location['latitude'] = location[0] if location else 'Unknown'
+        pc_location['longitude'] = location[1] if location else 'Unknown'
+        system_info['pc_location'] = pc_location
+
+        file_path = rf'{self.drive}\A.D.A\A.D.A\loadup\utils\system_info.json'
+        with open(file_path, 'w') as file:
+            json.dump(system_info, file, indent=4)
+
     
     def load_cNd(self):
         current_path = os.path.abspath(sys.argv[0])
         self.drive = os.path.splitdrive(current_path)[0]
-        self.cmds=f"{self.drive}/A.D.A/A.D.A/loadup/wake/art.bat"
-        self.execute()
+        self.cmds = f"{self.drive}/A.D.A/A.D.A/loadup/wake/art.bat"
+
+        gather_thread = threading.Thread(target=self.gather_system_info)
+        execute_thread = threading.Thread(target=self.execute)
+
+        gather_thread.start()
+        execute_thread.start()
+
+        gather_thread.join()
+        execute_thread.join()
 
     def execute(self):
         if "art.bat" in self.cmds:
